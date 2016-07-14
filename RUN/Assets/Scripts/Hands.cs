88958d731fd8d5684be52236374976b61618546b
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2016-07-15 00:47:01 +0100

    Handclimb test & improvements
    CHANGES
    - Changed fastclimb to handclimb
    - Minor cleanup in hands script
    - Changed checkstate of vertical-climb to improve transition between
    climbing and grabbing
    - Updated player collider sizes, currently testing different sizes for
    grabbing

--- RUN/Assets/Scripts/Hands.cs
+++ RUN/Assets/Scripts/Hands.cs
@@ -9,10 +9,12 @@ public class Hands : MonoBehaviour {
 
     public bool m_CanClimb;
 
-    bool m_HasSentMsg = false;
+    bool m_HasSentMsg;
 
     void Start ()
     {
+        m_HasSentMsg = false;
+
         m_Rigidbody = GetComponentInParent<Rigidbody>();
         m_CPlayer = GetComponentInParent<ControllerPlayer>();
         m_AnimHandler = GetComponentInParent<AnimationHandler>();
@@ -25,11 +27,9 @@ public class Hands : MonoBehaviour {
             //Climb if pressing space
             if (Input.GetKey(KeyCode.Space))
             {
-                m_Rigidbody.useGravity = true;
-
                 if (!m_HasSentMsg)
                 {
-                    m_CPlayer.SendMessage("FastClimb");
+                    m_CPlayer.SendMessage("HandClimb");
                     m_HasSentMsg = true;
                 }
 
@@ -52,22 +52,9 @@ public class Hands : MonoBehaviour {
                 m_Rigidbody.useGravity = false;
                 m_Rigidbody.velocity = Vector3.zero;
 
-                m_CPlayer.IsGrabbed(false);
-            }
-        }
-    }
-
-    void OnTriggerStay(Collider col)
-    {
-        if (col.GetComponent<ParkourObject>())
-        {
-            if (col.GetComponent<ParkourObject>().m_Climbable)
-            {
                 m_CanClimb = true;
-            }
-            else
-            {
-                m_CanClimb = false;
+                m_CPlayer.IsGrabbed(true);
+                Debug.Log("Grabbed");
             }
         }
     }
@@ -79,9 +66,9 @@ public class Hands : MonoBehaviour {
             if (col.GetComponent<ParkourObject>().m_Climbable)
             {
                 m_CanClimb = false;
-
                 m_Rigidbody.useGravity = true;
                 m_CPlayer.IsGrabbed(false);
+                Debug.Log("Released");
             }
         }
     }
