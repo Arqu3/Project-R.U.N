5e9c450e4accb1e5ef6e11a3239ab786cf060be1
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2016-07-27 13:36:39 +0100

    Menu and UI update
    ADDED
    - UI elements now scale with screen resolution, reference resolution for
    scaling is 1920x1080
    - Options button in main menu is now functional (animation, panel etc..)
    CHANGES
    - Animations in main menu now plays in either 1 or -1 direction, no
    longer uses separate animations for moving forward/backwards
    - Updated Canvas prefab to use the new scaling functionality

--- RUN/Assets/Scripts/MainMenuFuncs.cs
+++ RUN/Assets/Scripts/MainMenuFuncs.cs
@@ -27,18 +27,31 @@ public class MainMenuFuncs : MonoBehaviour
     public void OnLevelSelectButton()
     {
         m_Handler.SetState(MainMenuHandler.State.LevelSelect);
-        m_Animator.Play("MoveButtonsLevelSelect");
+        m_Animator.SetFloat("Direction", 1.0f);
+        m_Animator.Play("MoveButtonsLevelSelect", 0, 0.0f);
     }
 
     public void OnBackToMenuButton()
     {
+        //Set direction to -1 and normalized time to 1 to reverse animation
+        if (m_Handler.GetState().Equals(MainMenuHandler.State.LevelSelect))
+        {
+            m_Animator.SetFloat("Direction", -1.0f);
+            m_Animator.Play("MoveButtonsLevelSelect", 0, 1.0f);
+        }
+        else if (m_Handler.GetState().Equals(MainMenuHandler.State.Options))
+        {
+            m_Animator.SetFloat("Direction", -1.0f);
+            m_Animator.Play("MoveButtonsOptions", 0, 1.0f);
+        }
         m_Handler.SetState(MainMenuHandler.State.Main);
-        m_Animator.Play("MoveButtonsMain");
     }
 
     public void OnOptionsButton()
     {
         m_Handler.SetState(MainMenuHandler.State.Options);
+        m_Animator.SetFloat("Direction", 1.0f);
+        m_Animator.Play("MoveButtonsOptions", 0, 0.0f);
     }
 
     public void OnExitButton()
