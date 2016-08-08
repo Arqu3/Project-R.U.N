6b8cf6a1fee4c84765149c2bcd55f09686a30c34
ritli <agnar_oskarsson@hotmail.com>
2016-08-09 01:11:27 +0200

    Added 3 new models, added new test skybox, minor organizing
    ADDED
    - 3 New models, 2 Lamp variants, 1 small pillar
    - 4 New prefabs, 2 Lamp standalones, 1 pillar standalone, 1 pillar +
    lamp combo
    - New nightmode scene, purely for testing purposes at the moment
    CHANGES
    - Some organizing done in folders

---
+++ RUN/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public Sprite logoImage;
+
+	void Start () {
+        float range = Random.Range(0, 100);
+
+        if (range < 10)
+        {
+            GetComponent<Image>().sprite = logoImage;
+        }
+
+
+	}
+}
