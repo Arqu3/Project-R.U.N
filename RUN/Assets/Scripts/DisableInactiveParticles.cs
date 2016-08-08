84929541b43e6b415bcf248d918fadcf20af0761
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2016-08-08 01:42:49 +0100

    Particle system bug fix
    ADDED
    - DisableInactiveParticles script, checks if a particlesystem renderer
    should be enabled or not depending on how many active particles the
    system currently has
    CHANGES
    - Platforms with particle systems and camera now uses
    DisableInactiveParticles

---
+++ RUN/Assets/Scripts/DisableInactiveParticles.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class DisableInactiveParticles : MonoBehaviour
+{
+    ParticleSystem.Particle[] m_unused = new ParticleSystem.Particle[1];
+
+    void Awake()
+    {
+        GetComponent<ParticleSystemRenderer>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        GetComponent<ParticleSystemRenderer>().enabled = GetComponent<ParticleSystem>().GetParticles(m_unused) > 0;
+	}
+}
