f5f6a1d283b3d4dc530ad3d918bd36c14a8ef688
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2016-08-17 02:17:35 +0100

    Climb rotation update
    CHANGES
    - Changed some prefabs, updated some scripts
    Started working on setting player rotation when grabbing a ledge

--- RUN/Assets/Scripts/PlayerRotationHandler.cs
+++ RUN/Assets/Scripts/PlayerRotationHandler.cs
@@ -3,26 +3,32 @@ using System.Collections;
 
 public class PlayerRotationHandler : MonoBehaviour
 {
-
+    SimpleSmoothMouseLook m_Camera;
     ControllerPlayer m_Player;
 
     void Start()
     {
         m_Player = GetComponentInChildren<ControllerPlayer>();
+        m_Camera = Camera.main.GetComponent<SimpleSmoothMouseLook>();
     }
 
     void Update()
     {
         if (m_Player.GetIsControls())
         {
-            if (!m_Player.GetState().Equals(MovementState.Wallrunning))
+            if (!m_Player.GetState().Equals(MovementState.Wallrunning) && !m_Player.GetState().Equals(MovementState.Grabbing))
             {
-                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
-                Camera.main.GetComponent<SimpleSmoothMouseLook>().ClampMouseX(transform.forward, 360);
+                transform.localEulerAngles = new Vector3(0.0f, Camera.main.transform.localEulerAngles.y, 0.0f);
+                m_Camera.ClampMouseX(transform.forward, 360);
             }
-            else
+            else if (m_Player.GetState().Equals(MovementState.Wallrunning))
             {
                 transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, m_Player.GetWallrunDir(), 0.05f, 0f));
+            }
+            else if (m_Player.GetState().Equals(MovementState.Grabbing))
+            {
+                transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, m_Player.GetGrabbedDir(), 0.05f, 0f));
+                m_Camera.ClampMouseX(m_Player.GetGrabbedDir(), 200);
             }
         }
     }
