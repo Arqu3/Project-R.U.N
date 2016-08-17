42f8ac5e325a75cf96bc26da8996324301d2ab05
Arqu3 <ludwiggustavsson3@gmail.com>
2016-08-17 15:48:00 +0200

    Added disabletrigger
    ADDED
    - Prefab that toggles a gameobject on/off when entering its trigger

---
+++ RUN/Assets/Scripts/DisableGameObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableGameObject : MonoBehaviour
+{
+    public GameObject m_ToggleGameObject;
+
+    void Start()
+    {
+        if (!m_ToggleGameObject)
+        {
+            Debug.Log("DisableGameObject needs a gameobject");
+            enabled = false;
+            return;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (m_ToggleGameObject.activeSelf)
+                m_ToggleGameObject.SetActive(false);
+            else
+                m_ToggleGameObject.SetActive(true);
+        }
+    }
+}
